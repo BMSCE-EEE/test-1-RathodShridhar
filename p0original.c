7446fe9bc8f58da7aa5ccef96cbd8e19a85e5cd6
Rathod Shridhar <94944939+RathodShridhar@users.noreply.github.com>
2021-11-26 03:48:14 +0000

    p1

--- p0original.c
+++ p0original.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main(void)
+int main (void)
 {
   printf("RATHOD SHRIDHAR\n");
   return 0;
--- p1original.c
+++ p1original.c
@@ -1,2 +1,12 @@
-
+#include<stdio.h>
+int main(void)
+{
+  int a,b,sum;
+  printf("Enter the value of a \n");
+  scanf("%d",&a);
+  printf("Enter the value of b \n");
+  scanf("%d",&b);
+  sum=a+b;
+  printf(" the sum is %d", sum);
+  return 0;
 }
